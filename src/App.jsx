b768ccacf758da1f2a05f4a00a39e8aa6a1c220e
aashi27200-del <aashi27200@gmail.com>
2026-05-23 11:58:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      background: "linear-gradient(to bottom right, pink, hotpink)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+      flexDirection: "column"
+    }}>
+      <h1>💖 Talking Aashi 💖</h1>
+      <p>Romantic React Website Successfully Running!</p>
+    </div>
+  );
+}
